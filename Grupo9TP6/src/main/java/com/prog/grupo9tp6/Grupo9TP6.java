70d76411cbc31499a04e0ae49d6daf8a4f7e7a8b
Jessica Auriol <21082656+sjauriol@users.noreply.github.com>
2026-09-15 19:38:58 -0300

    commit inicial

---
+++ Grupo9TP6/src/main/java/com/prog/grupo9tp6/Grupo9TP6.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.prog.grupo9tp6;
+
+/**
+ *
+ * @author usuario
+ */
+public class Grupo9TP6 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
