e2bd488d85643f2a214148b4c77ab5aec7e12fa0
Gonzalo Martin Asis <110574223+Gonzamartin91@users.noreply.github.com>
2026-09-22 18:29:10 -0300

    editado el package

--- Grupo9TP6/src/main/java/com/prog/grupo9tp6/GestionProductos.java
+++ Grupo9TP6/src/main/java/com/prog/grupo9tp6/GestionProductos.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package tp6_gestionproductos;
+package com.prog.grupo9tp6;
 
 import java.awt.Component;
 import javax.swing.JComboBox;
