60299e46ffa44fb0ee913a68972a10bb60d06748
Jessica Auriol <21082656+sjauriol@users.noreply.github.com>
2026-09-22 18:26:15 -0300

    enum categoria

---
+++ Grupo9TP6/src/main/java/com/prog/grupo9tp6/Categoria.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.prog.grupo9tp6;
+
+/**
+ *
+ * @author jessi
+ */
+public enum Categoria {
+    COMESTIBLE,
+    LIMPIEZA,
+    PERFUMERIA
+}
